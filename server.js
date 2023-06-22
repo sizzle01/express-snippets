40d11f14b7ff1fa36374f02ed22ea7eb22145be9
sizzle01 <talktoaliyu@gmail.com>
2023-06-22 17:39:01 +0100

    i used ejs to render a view

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Test")
+const express = require("express");
+app = express();
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) =>{
+    console.log("Just testing server")
+    res.render("index")
+})
+
+app.listen(3000)
